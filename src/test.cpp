55364b441fc68ce68c10ea57313de2c430be0c1c
Marcin Olko <molko@google.com>
2025-11-19 15:37:10 +0000

    Improved clang-format-and-lint action and removed unnecessary code
    Signed-off-by: Marcin Olko <molko@google.com>

--- src/test.cpp
+++
@@ -1,7 +0,0 @@
-#include "openfeature/provider.h"
-
-std::unique_ptr<openfeature::ProviderEvuation<bool>> GetBooleanEvaluation(
-    const std::string_view flag, bool defaultValue,
-    const openfeature::EvaluationContext& ctx) {
-  return nullptr;
-}

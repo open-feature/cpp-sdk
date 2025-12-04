a27b4ee8cc438df723b9f59d7bf0d87d5e6febaf
NeaguGeorgiana23 <neagugeorgiana@google.com>
2025-12-04 14:17:43 +0000

    Fix comments.
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>

--- test/noop_provider_test.cpp
+++ test/noop_provider_test.cpp
@@ -4,25 +4,24 @@
 
 using namespace openfeature;
 
-class NoopProviderBooleanTest : public ::testing::Test,
-                                public ::testing::WithParamInterface<bool> {};
-
-// Test to verify the metadata returned by the provider
+// Test to verify the metadata returned by the provider.
 TEST(NoopProviderTest, ShouldReturnProviderNameForMetadata) {
   NoopProvider provider;
   const Metadata metadata = provider.GetMetadata();
   EXPECT_EQ(metadata.name, "Noop Provider");
 }
 
-// Test to verify the boolean evaluation returns the default value
+class NoopProviderBooleanTest : public ::testing::Test,
+                                public ::testing::WithParamInterface<bool> {};
+
+// Test to verify the boolean evaluation returns the default value.
 TEST_P(NoopProviderBooleanTest, BooleanEvaluationShouldReturnDefaultValue) {
   NoopProvider provider;
-  const std::string flagKey = "my-bool-flag";
-  const EvaluationContext ctx;
   const bool defaultValue = GetParam();
 
   const std::unique_ptr<BoolResolutionDetails> details =
-      provider.GetBooleanEvaluation(flagKey, defaultValue, ctx);
+      provider.GetBooleanEvaluation("my-bool-flag", defaultValue,
+                                    EvaluationContext{});
 
   EXPECT_EQ(details->GetValue(), defaultValue);
   EXPECT_EQ(details->GetReason(), Reason::kDefault);
@@ -33,10 +32,10 @@ TEST_P(NoopProviderBooleanTest, BooleanEvaluationShouldReturnDefaultValue) {
 }
 
 INSTANTIATE_TEST_SUITE_P(BooleanDefaultValues, NoopProviderBooleanTest,
-                         ::testing::Values(true, false));
+                         testing::Values(true, false));
 
-// Main function to initialize and run all tests
+// Main function to initialize and run all tests.
 int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
+  testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }

de996064e40a9c0d418afb0aa2cf56274c48d54f
NeaguGeorgiana23 <neagugeorgiana@google.com>
2025-12-04 09:44:00 +0000

    Fix comments.
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>

--- test/noop_provider_test.cpp
+++ test/noop_provider_test.cpp
@@ -4,50 +4,36 @@
 
 using namespace openfeature;
 
-class NoopProviderTest : public ::testing::Test {
- protected:
-  NoopProvider provider;
-};
+class NoopProviderBooleanTest : public ::testing::Test,
+                                public ::testing::WithParamInterface<bool> {};
 
 // Test to verify the metadata returned by the provider
-TEST_F(NoopProviderTest, ShouldReturnProviderNameForMetadata) {
+TEST(NoopProviderTest, ShouldReturnProviderNameForMetadata) {
+  NoopProvider provider;
   const Metadata metadata = provider.GetMetadata();
   EXPECT_EQ(metadata.name, "Noop Provider");
 }
 
 // Test to verify the boolean evaluation returns the default value
-TEST_F(NoopProviderTest, BooleanEvaluationShouldReturnDefaultValue) {
+TEST_P(NoopProviderBooleanTest, BooleanEvaluationShouldReturnDefaultValue) {
+  NoopProvider provider;
   const std::string flagKey = "my-bool-flag";
   const EvaluationContext ctx;
+  const bool defaultValue = GetParam();
 
-  // Test case with default value set to true
-  {
-    const bool defaultValue = true;
-    const std::unique_ptr<BoolResolutionDetails> details =
-        provider.GetBooleanEvaluation(flagKey, defaultValue, ctx);
+  const std::unique_ptr<BoolResolutionDetails> details =
+      provider.GetBooleanEvaluation(flagKey, defaultValue, ctx);
 
-    EXPECT_EQ(details->GetValue(), defaultValue);
-    EXPECT_EQ(details->GetReason(), Reason::kDefault);
-    EXPECT_EQ(details->GetVariant(), "default-variant");
-    EXPECT_FALSE(details->GetErrorCode().has_value());
-    ASSERT_TRUE(details->GetErrorMessage().has_value());
-    EXPECT_TRUE(details->GetErrorMessage()->empty());
-  }
-
-  // Test case with default value set to false
-  {
-    const bool defaultValue = false;
-    const std::unique_ptr<BoolResolutionDetails> details =
-        provider.GetBooleanEvaluation(flagKey, defaultValue, ctx);
-
-    EXPECT_EQ(details->GetValue(), defaultValue);
-    EXPECT_EQ(details->GetReason(), Reason::kDefault);
-    EXPECT_EQ(details->GetVariant(), "default-variant");
-    EXPECT_FALSE(details->GetErrorCode().has_value());
-    ASSERT_TRUE(details->GetErrorMessage().has_value());
-    EXPECT_TRUE(details->GetErrorMessage()->empty());
-  }
+  EXPECT_EQ(details->GetValue(), defaultValue);
+  EXPECT_EQ(details->GetReason(), Reason::kDefault);
+  EXPECT_EQ(details->GetVariant(), "default-variant");
+  EXPECT_FALSE(details->GetErrorCode().has_value());
+  ASSERT_TRUE(details->GetErrorMessage().has_value());
+  EXPECT_TRUE(details->GetErrorMessage()->empty());
 }
+
+INSTANTIATE_TEST_SUITE_P(BooleanDefaultValues, NoopProviderBooleanTest,
+                         ::testing::Values(true, false));
 
 // Main function to initialize and run all tests
 int main(int argc, char** argv) {

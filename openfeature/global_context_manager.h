240ac77e69786b0485afa03bfd5389e2c957a087
NeaguGeorgiana23 <neagugeorgiana@google.com>
2026-01-27 11:14:50 +0000

    feat: add global context manager
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>

---
+++ openfeature/global_context_manager.h
@@ -0,0 +1,34 @@
+#ifndef CPP_SDK_INCLUDE_OPENFEATURE_GLOBAL_CONTEXT_MANAGER_H_
+#define CPP_SDK_INCLUDE_OPENFEATURE_GLOBAL_CONTEXT_MANAGER_H_
+
+#include <memory>
+#include <shared_mutex>
+
+#include "openfeature/evaluation_context.h"
+
+namespace openfeature {
+
+// Manages the global Evaluation Context for the OpenFeature SDK.
+// This data is static across the application (unless explicitly changed)
+class GlobalContextManager {
+ public:
+  static GlobalContextManager& GetInstance();
+
+  GlobalContextManager(const GlobalContextManager&) = delete;
+  GlobalContextManager& operator=(const GlobalContextManager&) = delete;
+
+  // Updates the global evaluation context.
+  void SetGlobalEvaluationContext(const EvaluationContext& ctx);
+
+  // Retrieves the current global evaluation context.
+  EvaluationContext GetGlobalEvaluationContext() const;
+
+ private:
+  GlobalContextManager() = default;
+  EvaluationContext global_evaluation_context_;
+  mutable std::shared_mutex context_mutex_;
+};
+
+}  // namespace openfeature
+
+#endif  // CPP_SDK_INCLUDE_OPENFEATURE_GLOBAL_CONTEXT_MANAGER_H_

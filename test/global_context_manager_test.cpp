0e7afa6033189d895cc9d1b2a96ebcdd94a115e6
NeaguGeorgiana23 <115723925+NeaguGeorgiana23@users.noreply.github.com>
2026-01-30 09:40:23 +0100

    Global API (#54)
    * feat: add global context manager
    
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>
    
    * feat: add client api and boolean evaluation
    
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>
    
    * feat: add OpenFeature API singleton and update ProviderRepository shutdown logic to re-initialize everithing to the default state.
    
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>
    
    * apply gemini suggestions.
    
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>
    
    ---------
    
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>
    Signed-off-by: NeaguGeorgiana23 <115723925+NeaguGeorgiana23@users.noreply.github.com>

--- test/global_context_manager_test.cpp
+++ test/global_context_manager_test.cpp
@@ -82,8 +82,3 @@ TEST_F(GlobalContextManagerTest, ThreadSafetyStressTest) {
     t.join();
   }
 }
-
-int main(int argc, char** argv) {
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}

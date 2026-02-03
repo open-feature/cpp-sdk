d87c477a6a35c6ecd770859187ac4f25a9760285
Marcin Olko <molko@google.com>
2026-02-03 11:07:52 +0000

    Checking if lines matter
    Signed-off-by: Marcin Olko <molko@google.com>

--- openfeature/evaluation_context.h
+++ openfeature/evaluation_context.h
@@ -53,6 +53,8 @@ class EvaluationContext::Builder {
   // The build() method creates the final, immutable EvaluationContext object.
   EvaluationContext build() const;
 
+  int Testing;
+
  private:
   std::optional<std::string> targeting_key_;
   std::map<std::string, std::any> attributes_;

bdb2b4ceca82b5efcc97e150d7395103c83a5f12
NeaguGeorgiana23 <115723925+NeaguGeorgiana23@users.noreply.github.com>
2026-02-16 13:47:50 +0100

    Implement in_memory_provider. (#61)
    * Implement in_memory_provider.
    
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>
    
    * Fix comments
    
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>
    
    * Resolve comments.
    
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>
    
    ---------
    
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>

---
+++ openfeature/memory_provider/flag.h
@@ -0,0 +1,58 @@
+#ifndef CPP_SDK_INCLUDE_OPENFEATURE_MEMORY_PROVIDER_FLAG_H_
+#define CPP_SDK_INCLUDE_OPENFEATURE_MEMORY_PROVIDER_FLAG_H_
+
+#include <functional>
+#include <optional>
+#include <string>
+#include <unordered_map>
+#include <utility>
+
+#include "absl/status/statusor.h"
+#include "openfeature/evaluation_context.h"
+#include "openfeature/flag_metadata.h"
+
+namespace openfeature {
+
+// This class represents the flag structure used for the InMemoryProvider.
+template <typename T>
+class Flag {
+ public:
+  using ContextEvaluator =
+      std::function<absl::StatusOr<T>(const Flag&, const EvaluationContext&)>;
+
+  Flag(std::unordered_map<std::string, T> variants,
+       std::optional<std::string> default_variant,
+       ContextEvaluator context_evaluator, FlagMetadata flag_metadata,
+       bool disabled = false)
+      : variants_(std::move(variants)),
+        default_variant_(std::move(default_variant)),
+        context_evaluator_(std::move(context_evaluator)),
+        flag_metadata_(std::move(flag_metadata)),
+        disabled_(disabled) {}
+
+  const std::unordered_map<std::string, T>& GetVariants() const {
+    return variants_;
+  }
+
+  const std::optional<std::string>& GetDefaultVariant() const {
+    return default_variant_;
+  }
+
+  const ContextEvaluator& GetContextEvaluator() const {
+    return context_evaluator_;
+  }
+
+  const FlagMetadata& GetFlagMetadata() const { return flag_metadata_; }
+
+  bool IsDisabled() const { return disabled_; }
+
+ private:
+  std::unordered_map<std::string, T> variants_;
+  std::optional<std::string> default_variant_;
+  ContextEvaluator context_evaluator_;
+  FlagMetadata flag_metadata_;
+  bool disabled_;
+};
+}  // namespace openfeature
+
+#endif  // CPP_SDK_INCLUDE_OPENFEATURE_MEMORY_PROVIDER_FLAG_H_

021f03f92df8d171acfc39832252a91ce67f65b2
NeaguGeorgiana23 <115723925+NeaguGeorgiana23@users.noreply.github.com>
2026-02-19 10:34:00 +0100

    Update SDK to allow evaluation for more than just boolean flags. (#64)
    * Update SDK to allow evaluation for more than just boolean flags.
    
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>
    
    * Fix comments and add unit tests
    
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>
    
    * Fix comments and add unit tests
    
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>
    
    * Update Mrege Logic
    
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>
    
    ---------
    
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>

--- openfeature/client_api.h
+++ openfeature/client_api.h
@@ -16,6 +16,7 @@
 #include "openfeature/provider_repository.h"
 #include "openfeature/provider_status.h"
 #include "openfeature/resolution_details.h"
+#include "openfeature/value.h"
 
 namespace openfeature {
 
@@ -40,19 +41,62 @@ class ClientAPI : public Client {
   // Returns the current status of the associated provider.
   ProviderStatus GetProviderStatus() override;
 
+  // Evaluate a boolean flag.
   bool GetBooleanValue(std::string_view flag_key, bool default_value) override;
   bool GetBooleanValue(std::string_view flag_key, bool default_value,
                        const EvaluationContext& ctx) override;
 
+  // Evaluate a string flag.
+  std::string GetStringValue(std::string_view flag_key,
+                             std::string_view default_value) override;
+  std::string GetStringValue(std::string_view flag_key,
+                             std::string_view default_value,
+                             const EvaluationContext& ctx) override;
+
+  // Evaluate an integer flag.
+  int64_t GetIntegerValue(std::string_view flag_key,
+                          int64_t default_value) override;
+  int64_t GetIntegerValue(std::string_view flag_key, int64_t default_value,
+                          const EvaluationContext& ctx) override;
+  // Evaluate a double flag.
+  double GetDoubleValue(std::string_view flag_key,
+                        double default_value) override;
+  double GetDoubleValue(std::string_view flag_key, double default_value,
+                        const EvaluationContext& ctx) override;
+  // Evaluate an object flag.
+  Value GetObjectValue(std::string_view flag_key, Value default_value) override;
+  Value GetObjectValue(std::string_view flag_key, Value default_value,
+                       const EvaluationContext& ctx) override;
+
   // TODO: Add methods to get and set Hooks.
-  // TODO: Add methods for flag evaluation for other types (e.g. string, int,
-  // float, object).
   // TODO: Add methods for detailed flag evaluation.
   // TODO: Overload method "GetBooleanValue" to accept "Evaluation Options".
 
  private:
+  template <typename ResolutionDetailsType, typename ValueType,
+            typename ProviderCallable>
+  std::unique_ptr<ResolutionDetailsType> EvaluateFlag(
+      ValueType default_value, const std::optional<EvaluationContext>& ctx,
+      ProviderCallable provider_call);
+
   std::unique_ptr<BoolResolutionDetails> EvaluateBooleanFlag(
       std::string_view flag_key, bool default_value,
+      const std::optional<EvaluationContext>& ctx);
+
+  std::unique_ptr<StringResolutionDetails> EvaluateStringFlag(
+      std::string_view flag_key, std::string_view default_value,
+      const std::optional<EvaluationContext>& ctx);
+
+  std::unique_ptr<IntResolutionDetails> EvaluateIntegerFlag(
+      std::string_view flag_key, int64_t default_value,
+      const std::optional<EvaluationContext>& ctx);
+
+  std::unique_ptr<DoubleResolutionDetails> EvaluateDoubleFlag(
+      std::string_view flag_key, double default_value,
+      const std::optional<EvaluationContext>& ctx);
+
+  std::unique_ptr<ObjectResolutionDetails> EvaluateObjectFlag(
+      std::string_view flag_key, Value default_value,
       const std::optional<EvaluationContext>& ctx);
 
   EvaluationContext MergeContexts(
@@ -63,6 +107,29 @@ class ClientAPI : public Client {
   EvaluationContext evaluation_context_;
   mutable std::mutex context_mutex_;
 };
+
+template <typename ResolutionDetailsType, typename ValueType,
+          typename ProviderCallable>
+std::unique_ptr<ResolutionDetailsType> ClientAPI::EvaluateFlag(
+    ValueType default_value, const std::optional<EvaluationContext>& ctx,
+    ProviderCallable provider_call) {
+  if (GetProviderStatus() != ProviderStatus::kReady) {
+    return std::make_unique<ResolutionDetailsType>(
+        default_value, Reason::kError, std::nullopt, FlagMetadata(),
+        ErrorCode::kProviderNotReady, "Provider is not ready");
+  }
+
+  std::shared_ptr<FeatureProvider> provider =
+      provider_repository_.GetProvider(domain_);
+  if (!provider) {
+    return std::make_unique<ResolutionDetailsType>(
+        default_value, Reason::kError, std::nullopt, FlagMetadata(),
+        ErrorCode::kProviderFatal, "Provider not found for domain");
+  }
+
+  EvaluationContext merged_context = MergeContexts(ctx);
+  return provider_call(provider, merged_context);
+}
 
 }  // namespace openfeature
 

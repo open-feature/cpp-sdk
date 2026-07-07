42a776da7900676c95d51ca345f06c8068560337
NeaguGeorgiana23 <neagugeorgiana@google.com>
2026-07-07 13:19:57 +0000

    base data structures fot hook implementation.
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>

---
+++ openfeature/flag_type_value.h
@@ -0,0 +1,15 @@
+#ifndef CPP_SDK_INCLUDE_OPENFEATURE_FLAG_VALUE_TYPE_H_
+#define CPP_SDK_INCLUDE_OPENFEATURE_FLAG_VALUE_TYPE_H_
+
+namespace openfeature {
+
+enum class FlagValueType {
+  kBoolean,
+  kString,
+  kInteger,
+  kDouble,
+  kObject
+};
+
+}  // namespace openfeature
+#endif  // CPP_SDK_INCLUDE_OPENFEATURE_FLAG_VALUE_TYPE_H_

334ec2a64d9a7a0b4362289a2f8d16405b3ce855
NeaguGeorgiana23 <neagugeorgiana@google.com>
2026-07-07 13:56:50 +0000

    fix linter
    Signed-off-by: NeaguGeorgiana23 <neagugeorgiana@google.com>

--- openfeature/flag_type_value.h
+++ openfeature/flag_type_value.h
@@ -1,9 +1,17 @@
 #ifndef CPP_SDK_INCLUDE_OPENFEATURE_FLAG_VALUE_TYPE_H_
 #define CPP_SDK_INCLUDE_OPENFEATURE_FLAG_VALUE_TYPE_H_
 
+#include <cstdint>
+
 namespace openfeature {
 
-enum class FlagValueType { kBoolean, kString, kInteger, kDouble, kObject };
+enum class FlagValueType : std::uint8_t {
+  kBoolean,
+  kString,
+  kInteger,
+  kDouble,
+  kObject
+};
 
 }  // namespace openfeature
 #endif  // CPP_SDK_INCLUDE_OPENFEATURE_FLAG_VALUE_TYPE_H_
